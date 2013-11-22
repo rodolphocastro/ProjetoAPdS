ed5bc15d459e0c9acd761ed808a9e1322171654f
AlvesRC <AlvesRC@RC-Notebook>
2013-11-22 15:10:16 -0200

    Pre Alpha
    + Adicionada a biblioteca SQLite ao GIT;
    + Atualizada a classe ErrorHandler, agora implementando a interface Errors

--- src/controle/ErrorHandler.java
+++ src/controle/ErrorHandler.java
@@ -4,6 +4,6 @@ package controle;
  * Classe responsável por lidar com erros, emitir relatórios e erros e impedir maiores danos.
  * @author ALVES, R.C.
  */
-public class ErrorHandler {
+public class ErrorHandler implements Errors{
     
 }
